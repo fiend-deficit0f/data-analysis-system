5a8e027fcf1c846cb179905ec67d48882bb9b4b0
Abigail Parker <153265183+fiend-deficit0f@users.noreply.github.com>
2023-12-08 14:09:51 +0700

    Create visualization.py

---
+++ visualization.py
@@ -0,0 +1,11 @@
+# visualization.py
+
+import matplotlib.pyplot as plt
+
+def plot_data(data):
+    # Add data plotting logic here
+    plt.plot(data)
+    plt.xlabel('Data Points')
+    plt.ylabel('Values')
+    plt.title('Data Visualization')
+    plt.show()
